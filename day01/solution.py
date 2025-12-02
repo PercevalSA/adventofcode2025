10ccc9e91c949bbf19dd9cb5929aef01bae3b383
PercevalSA <perceval.arenou@ptxag.com>
2025-12-02 23:22:31 +0100

    first exo  of first day done

--- day01/solution.py
+++ day01/solution.py
@@ -1,15 +1,42 @@
+start_position: int = 50
+
+# The actual password is the number of times the dial is left pointing at 0
+# after any rotation in the sequence.
+
+
+def move_to_next_position(current_position: int, rotation: str) -> int:
+    direction = rotation[:1]
+    click_number = int(rotation[1:])
+    if direction == "L":
+        click_number = -click_number
+
+    current_position = (current_position + click_number) % 100
+
+    return current_position
+
+
 def part1(data: str) -> int:
-    lines = data.strip().split('\n')
-    result = lines
+    rotations = data.strip().split("\n")
+    dial_position = start_position
+    result = 0
+
+    for rotation in rotations:
+        dial_position = move_to_next_position(dial_position, rotation)
+
+        if dial_position == 0:
+            result += 1
+
     return result
 
+
 def part2(data: str) -> int:
-    lines = data.strip().split('\n')
+    lines = data.strip().split("\n")
     result = lines
-    return result
+    return len(result)
+
 
 if __name__ == "__main__":
     with open("input.txt") as f:
         data = f.read()
     print(f"Part 1: {part1(data)}")
-    print(f"Part 2: {part2(data)}") 
+    print(f"Part 2: {part2(data)}")
--- day01/test_solution.py
+++ day01/test_solution.py
@@ -1,9 +1,22 @@
-from solution import part1, part2
+from pathlib import Path
+
+from solution import move_to_next_position, part1
+
+sample_file: Path = Path(__file__).parent / "test_input.txt"
+
+
+def test_move_to_next_position():
+    assert move_to_next_position(50, "L10") == 40
+    assert move_to_next_position(40, "R20") == 60
+    assert move_to_next_position(60, "L70") == 90
+    assert move_to_next_position(90, "R15") == 5
+    assert move_to_next_position(5, "L5") == 0
 
 
 def test_part1():
-    sample = "example\ndata"
+    sample = sample_file.read_text()
     assert part1(sample) == 3
+
 
 # def test_part2():
 #     sample = "example\ndata"
